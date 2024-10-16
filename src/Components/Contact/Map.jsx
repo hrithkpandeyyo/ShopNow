6222ead0b642a1ba2b30632f61bc64511cbbbb27
hrithkpandeyyo <hrithikpandeyyo@gmail.com>
2024-10-17 02:08:53 +0530

    first commit

--- src/Components/Contact/Map.jsx
+++ src/Components/Contact/Map.jsx
@@ -4,16 +4,7 @@ export default function Map() {
 
   return (
     <div className="map">
-      <iframe
-        title="JANNAT VILLA SOLUTIONS"
-        src="https://maps.app.goo.gl/s6bQqLPmRj8Xm13P9"
-        width={600}
-        height={450}
-        style={{ border: 0 }}
-        allowFullScreen
-        loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"
-      />
+      <iframe height={450} width={600} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1999.5135750851434!2d76.64567958306347!3d30.755088673553022!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ff1f5cfc8ec4d%3A0xc73cb643eab79be2!2sJANNAT%20VILLA!5e1!3m2!1sen!2sin!4v1729111070375!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
   );
 }
