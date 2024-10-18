423d3996858b62934a92d7f3b642ea5d30668f6a
hrithkpandeyyo <hrithikpandeyyo@gmail.com>
2024-10-18 19:03:03 +0530

    init

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
           </Routes>
           <News />
           <Footer />
-          <Copyright />
+          {/* <Copyright /> */}
         </DataProvider>
       </CartData>
     </BlogData>
